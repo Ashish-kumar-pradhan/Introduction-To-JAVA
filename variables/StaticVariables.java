1c8926a237a95246bc2d5a9e1e4f088bad19fa20
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-10-28 18:45:44 +0530

    Static Variables

---
+++ variables/StaticVariables.java
@@ -0,0 +1,16 @@
+package variables;
+
+class test{
+	static int a = 10;
+}
+
+public class StaticVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(test.a);
+
+	}
+
+}
